e60a5f0af6f1146c8d78dba5cb236c83ec56f805
Ernest Ampem <ernestampem12008@gmail.com>
2024-08-26 19:48:43 +0000

    updated css

---
+++ src/Menu.js
@@ -0,0 +1,11 @@
+export default function Menu(){
+    return(
+        <div className="menu-bottom">
+            <div className="menu-item">Home</div>
+            <div className="menu-item">Savings</div>
+            <div className="menu-item">Budget</div>
+
+        </div>
+
+    )
+}
