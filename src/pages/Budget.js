5d53050a23147ebb9e6c7c1b1c46ea529f2a6fb0
Ernest Ampem <ernestampem12008@gmail.com>
2024-09-10 07:38:09 +0000

    added a few updates

---
+++ src/pages/Budget.js
@@ -0,0 +1,12 @@
+export default function Budget(){
+    return(
+        <>
+        <h1>Monthly budget</h1>
+        <p>To save: </p>
+        <p>To spend: </p>
+        <p>Estimated savings</p>
+
+
+        </>
+    )
+}
