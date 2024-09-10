6996a6ad331cad7ed86bf2989ba3a8dbd4e79dac
Ernest Ampem <ernestampem12008@gmail.com>
2024-09-10 07:02:35 +0000

    updated the savingscalculator component and started with the paged route

--- src/models/Savingscalculator.js
+++ src/models/Savingscalculator.js
@@ -1,34 +1,41 @@
 import { useState } from "react"
 
-export default function Calculator(){
-    const [days, setDays] = useState(0)
-    const [amount, setAmount] = useState(0)
-    const [result, setResult] = useState(0)
+export default function Calculator() {
+    const [days, setDays] = useState(0);
+    const [amount, setAmount] = useState(0);
+    const [result, setResult] = useState(0);
 
-    const updateAmount = ()=>{
-        let master = document.querySelector("#daily_amount")
-        if(master){
-            setAmount(parseFloat(master.value) || 0);
-        }
+    const updateAmount = (e) => {
+        const newAmount = parseFloat(e.target.value) || 0;
+        setAmount(newAmount);
+        setResult(newAmount * days);
     }
 
-    const updateResult = ()=>{
-        let daysInput = document.querySelector("#daysnum");
-        if(daysInput){
-            setDays(parseFloat(daysInput.value) || 0);
-        }
-        setResult((days* amount));
-    }
+    const updateDays = (e) => {
+        const newDays = parseFloat(e.target.value) || 0;
+        setDays(newDays);
+        setResult(newDays * amount); 
+    };
 
-    return(
-        
+    return (
         <div id="calculator">
-        <h3>Savings calculator</h3>
-        <input type="number" placeholder="enter amount to save daily" id="daily_amount" onChange={updateAmount}></input>
-        <input type="number" placeholder="enter the number of days" id="daysnum" onChange={updateResult}></input>
-        <p>Result: GHS{result}</p>
-
-        <h5>You will have GHS{result} if you save {amount} everyday for {days} days</h5>
+            <h3>Savings calculator</h3>
+            <input 
+                type="number" 
+                placeholder="Enter amount to save daily" 
+                id="daily_amount" 
+                onChange={updateAmount}>
+            </input>
+            <input 
+                type="number" 
+                placeholder="Enter the number of days" 
+                id="daysnum" 
+                onChange={updateDays}>
+            </input>
+            <p>Result: GHS {result}</p>
+            <h5>
+                You will have GHS {result} if you save {amount} every day for {days} days
+            </h5>
         </div>
-    )
+    );
 }
